6c808ab6862d0aa2f5461eaecdb0c8e4d8feafc9
krivenkop <ukr.empresario@gmail.com>
2020-03-11 00:41:00 +0200

    ADD: config for auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users,
              path: '',
              path_names: {
-                 sign_in: 'login',
-                 sign_out: 'logout',
                  registration: 'signup'
              },
-             controllers: {
-                 sessions: 'users/sessions',
-                 registrations: 'users/registrations'
-             }
+             :skip => [:sessions, :registration]
+  as :user do
+    post "/signin" => "users/sessions#create", :as => :user_session
+    delete "/logout" => "users/sessions#destroy", :as => :destroy_user_session
+    post "/signup" => "users/registrations#create"
+  end
 
 
   root to: 'application#index'
