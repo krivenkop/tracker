3fac6d2e3f3958ebb80a19bb4b75b17254c6ff07
krivenkop <ukr.empresario@gmail.com>
2020-03-11 11:22:24 +0200

    CHANGE: markup for login page

--- app/javascript/packs/components/pages/Login.jsx
+++ app/javascript/packs/components/pages/Login.jsx
@@ -8,10 +8,23 @@ export default () => {
   return (
     <div className="login-form__wrapper">
       <form action="/login" className="login-form" onSubmit={processSubmit}>
-        <input type="text" className="login-form__username"/>
-        <input type="password" className="login-form__password"/>
+        <h1 className="login-form__title">Login</h1>
 
-        <button className="login-form__submit">Войти</button>
+        <div className="form__group">
+          <label htmlFor="username" className="form__label">Username</label>
+          <input type="text" className="login-form__username form__string"
+                 name="username" id="username" />
+        </div>
+
+        <div className="form__group">
+          <label htmlFor="password" className="form__label">Password</label>
+          <input type="password" className="login-form__password form__string"
+                 name="password" id="password" />
+        </div>
+
+        <div className="d-flex justify-content-center">
+          <button className="login-form__submit form__submit">Войти</button>
+        </div>
       </form>
     </div>
   );
