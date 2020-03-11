b704ff68a8fc5ffbaed45dfc7150fdb420c39f29
krivenkop <ukr.empresario@gmail.com>
2020-03-11 11:29:27 +0200

    ADD: annotate

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: jwt_blacklist
+#
+#  id         :bigint           not null, primary key
+#  jti        :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class JwtBlacklist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Blacklist
 
