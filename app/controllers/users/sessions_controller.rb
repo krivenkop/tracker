34f5969d3e1f50e7267eb22e382d24b695612303
krivenkop <ukr.empresario@gmail.com>
2020-03-15 22:36:05 +0200

    ADD: specs for auth controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,43 @@
 # frozen_string_literal: true
 
+
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
-  private
+  def create
+    unless authenticate(user_params)
+      render json: { success: false }, status: :forbidden
+      return
+    end
 
-  def respond_with(resource, _opts = {})
-    render json: resource
+    jwt_auth = JwtAuth.new(
+        secret: Rails.application.credentials.secret_key_base,
+        refresh_lifetime: 48.hours,
+        access_lifetime: 30.minutes
+    )
+    tokens = jwt_auth.authenticate(@user)
+
+    response = {
+        jwt: {
+            access: tokens[:access],
+            refresh: tokens[:refresh]
+        },
+        payload: { user: @user }
+    }
+
+    render json: response
   end
 
-  def respond_to_on_destroy
-    head :no_content
+  private
+
+  def authenticate(data)
+    @user = User.find_by(email: data[:email])
+    return false if @user.nil?
+
+    @user&.valid_password?(data[:password])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
